da07bbb79cc3cfbd1874503e9db749f6dc4847f1
Maulik <shahms95@gmail.com>
2019-04-15 19:09:52 -0500

    changed the paths to correct values

--- AlexNet/scripts/train.py
+++ AlexNet/scripts/train.py
@@ -12,10 +12,10 @@ import numpy as np
 
 import tensorflow as tf
 
-from .. import datasets
-from ..nets import alexnetmodes
-from ..utils import tfhelper
-from ..utils import misc
+from ../ import datasets
+from ../nets import alexnetmodes
+from ../utils import tfhelper
+from ../utils import misc
 
 
 net_configs = {
